9e18950b2ee013d69510141744e43f8cc8fc2c7b
TrenchHome <trenchhome@gmail.com>
2023-03-02 21:46:20 +0800

    add source code

---
+++ master_microphone/microphone.ino
@@ -0,0 +1,62 @@
+#include <SoftwareSerial.h>   // 引用程式庫
+SoftwareSerial BT(10, 11); // 接收腳, 傳送腳
+#define inputPin A5  // selecting the analog input pin
+#define DIFFERENCE 500
+#define TrashTimes 10000
+#define SWITCH 'K'
+#define DONE 'R'
+unsigned int inputSample; // placeholder for a single measurement
+unsigned int inputMax;
+unsigned int inputMin;
+int diff;
+
+void setup() {
+  Serial.begin(9600);   // 與電腦序列埠連線
+  BT.begin(9600);
+}
+
+void loop() {
+  char val = '\0';  // 儲存接收資料的變數
+  
+  _readSound();  // loop for the window
+  _displayDiff(); // send the values on serial
+  
+  if (diff > DIFFERENCE) {
+    BT.print(SWITCH);
+    delay(500);
+  }
+  
+  if (BT.available()) {
+    val = BT.read();
+    if (val == DONE) 
+      _readSound();
+  }
+  delay(500);
+}
+
+void _readSound() {
+  inputMax = 0;
+  inputMin = 1024;
+  diff = 0;
+  for (unsigned int i = 0; i < TrashTimes; i++) {
+    inputSample = analogRead(inputPin); // read in a single value
+    inputMin = min(inputMin, inputSample);  // get the minimum and maximum value
+    inputMax = max(inputMax, inputSample);
+  }
+}
+
+void _displayDiff() {
+  Serial.println("---");
+  diff = inputMax - inputMin;
+  Serial.print("\tTarget\t:\t");
+  Serial.println(DIFFERENCE);
+  Serial.print("\tNow\t:\t");
+  Serial.println(diff);
+  if (diff > DIFFERENCE) {
+    Serial.println(">>\tTurn On/ Off");  
+  }
+  else {
+    Serial.println(">>>\tLouder !!!");
+  }
+  Serial.println("---");
+}
---
+++ slave_motor/servomotor.ino
@@ -0,0 +1,54 @@
+#include <SoftwareSerial.h>
+#include <Wire.h>  //引用二個函式庫SoftwareSerial及Wire SoftwareSerial 
+#include <Servo.h>
+#define ServoPin 9
+#define SWITCH 'K'
+#define DONE 'R'
+SoftwareSerial I2CBT(10, 11); //定義PIN10及PIN11分別為TX及RX腳位
+Servo serv;
+int type = 0;
+char val;  // 儲存接收資料的變數
+
+void setup() {
+  Serial.begin(9600); //Arduino鮑率
+  I2CBT.begin(38400); //藍牙鮑率
+  serv.attach(ServoPin);
+  serv.write(77);
+}
+
+void loop() {
+  val = '\0';
+  
+  if (I2CBT.available()) { //判斷訊息接收
+    val = I2CBT.read();
+    ///////////////////////_displayType ();////////////////////////
+    if ( val == SWITCH )
+      _switch();
+  }
+  
+  delay(1000);
+}
+
+void _switch () { //type = 0 : 順時鐘轉 (正對sevo, 線在左方) 
+                  //type = 1 : 逆時鐘轉
+  int pos;
+  if (type) {
+    for (pos = 80; pos >= 55 ; pos -= 4)
+      serv.write(pos);
+    delay(300);
+    serv.write(77);
+  }
+  else {
+    for (pos = 110; pos <= 135 ; pos += 4)
+      serv.write(pos);
+    delay(300);
+    serv.write(100);
+  }
+  type = (type + 1) % 2;
+  I2CBT.print(DONE);
+}
+
+void _displayType () {
+  Serial.print("Available. type= ");
+  Serial.println(type);
+}
